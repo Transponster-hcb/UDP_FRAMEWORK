56a367e03dda73dff488f61f53e95a74cd833929
hcb <huchuanbo@bjtu.edu.cn>
2025-09-18 11:02:29 +0800

    add main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "common.h"
+#include <pthread.h>
+#include "network.h"
+#include <unistd.h>
+
+#ifdef CONTROLLER_MODE//controller模式,加载配置文件，启动线程执行逻辑
+int main() {
+
+   
+
+}
+#endif
+
+
+#ifdef CONTROLLED_MODE//controlled模式,加载配置文件，启动线程执行逻辑
+
+int main() {
+
+   
+
+}
+#endif
